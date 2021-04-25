cefa0182b49136e197bb342aa096e2285ffa847c
Barnabás Balogh <barnabas.balogh@stud.uni-corvinus.hu>
2021-04-25 19:44:54 +0200

    9. gyakorlat

--- HajosTeszt/Controllers/BoatController.cs
+++ HajosTeszt/Controllers/BoatController.cs
@@ -8,18 +8,22 @@ using System.Threading.Tasks;
 
 namespace HajosTeszt.Controllers
 {
-   // [Route("api/[controller]")]
+    //[Route("api/[controller]")]
     [ApiController]
     public class BoatController : ControllerBase
     {
         [HttpGet]
-        [Route("questions/all")]
-        public ActionResult M1()
+        [Route("questions/{sorszám}")]
+        public ActionResult M1(int sorszám)
         {
             HajostesztContext context = new HajostesztContext();
-            var kérdések = from x in context.Questions select x.QuestionText;
+            var kérdés = (from x in context.Questions
+                          where x.QuestionId == sorszám
+                          select x).FirstOrDefault();
 
-            return new JsonResult(kérdések);
+            if (kérdés == null) return BadRequest("Nincs ilyen sorszámú kérdés");
+
+            return new JsonResult(kérdés);
         }
     }
 }
